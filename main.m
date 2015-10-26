e07592221cb70687193e18b31dc54f41c417003b
Michal Radecki <odrzutowiec@gmail.com>
2015-10-26 22:04:03 +0100

    polish characters

--- main.m
+++ main.m
@@ -2,7 +2,7 @@
 
 int main() {
     Person *brad = [[Person alloc] init];
-    brad.name = [@"Brad Cox" cString];
+    brad.name = (char *) [@"ść ąę polskie znaki ok" cString];
     [brad sayHello];
     [brad retain];
 }
